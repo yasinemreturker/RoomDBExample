607e66e95c6641a9ef7962bc613e9044619122e6
byTurker <mr_turker@hotmail.com>
2021-12-25 22:17:33 +0300

    Add user input validations

--- app/src/main/java/com/example/roomdbexample/viewmodels/SubscriberViewModel.kt
+++ app/src/main/java/com/example/roomdbexample/viewmodels/SubscriberViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.roomdbexample.viewmodels
 
+import android.util.Patterns
 import androidx.databinding.Observable
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -32,16 +33,24 @@ class SubscriberViewModel(private val repository: SubscriberRepository) : ViewMo
     }
 
     fun saveOrUpdate(){
-        if(isUpdateOrDelete){
-            subscriberToUpdateOrDelete.name = inputName.value!!
-            subscriberToUpdateOrDelete.email = inputEmail.value!!
-            update(subscriberToUpdateOrDelete)
-        }else {
-            val name = inputName.value!!
-            val email = inputEmail.value!!
-            insert(Subscriber(0, name, email))
-            inputName.value = null
-            inputEmail.value = null
+        if (inputName.value == null) {
+            statusMessage.value = Event("Please enter subscriber's name")
+        } else if (inputEmail.value == null) {
+            statusMessage.value = Event("Please enter subscriber's email")
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(inputEmail.value!!).matches()) {
+            statusMessage.value = Event("Please enter a correct email address")
+        } else {
+            if (isUpdateOrDelete) {
+                subscriberToUpdateOrDelete.name = inputName.value!!
+                subscriberToUpdateOrDelete.email = inputEmail.value!!
+                update(subscriberToUpdateOrDelete)
+            } else {
+                val name = inputName.value!!
+                val email = inputEmail.value!!
+                insert(Subscriber(0, name, email))
+                inputName.value = null
+                inputEmail.value = null
+            }
         }
     }
 
